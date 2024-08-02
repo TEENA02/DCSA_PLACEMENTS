7c96d7f068e0cf180171c4b788b7fcae85d9e038
TEENA02 <teenasajwan09@gmail.com>
2024-08-02 13:10:42 +0530

    changing the route

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import Placemnet_25 from '../components/Placemnet_25.jsx'
 const router=createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout/>}>
-  <Route index element={<Navigate to="/home" />} />
+  <Route index element={<Home />} />
       <Route path='home' element={<Home/>}/>
       <Route path='placement_23' element={<Placement_23/>}/>
       <Route path='placement_24' element={<Placement_24/>}/>
