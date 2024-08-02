1813eff21abd927e360720935da98f70fd7c8644
TEENA02 <teenasajwan09@gmail.com>
2024-08-02 13:08:30 +0530

    changing the route

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-<>  <div className="flex flex-col min-h-screen">
+    <>
+   
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    </>
+  );
+};
 
-    <Header/>
-    <Outlet/>
-    <Footer/>
-    </div>
-</>
-  )
-}
-
-export default Layout
+export default Layout;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider ,Navigate} from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from '../components/Home.jsx'
 import Placement_23 from '../components/Placement_23.jsx'
@@ -14,6 +14,7 @@ import Placemnet_25 from '../components/Placemnet_25.jsx'
 const router=createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout/>}>
+  <Route index element={<Navigate to="/home" />} />
       <Route path='home' element={<Home/>}/>
       <Route path='placement_23' element={<Placement_23/>}/>
       <Route path='placement_24' element={<Placement_24/>}/>
@@ -22,7 +23,7 @@ const router=createBrowserRouter(
   )
 )
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+
   <RouterProvider router={router}/>
 
 )
